520377eb16e33beb8a429f914828468c21d1a1ba
Ian Dickinson <ian.dickinson@epimorphics.com>
2018-10-15 13:19:46 +0100

    Solution: pass the new Selenium port as part of the test_settings as well

--- nightwatch.config.js
+++ nightwatch.config.js
@@ -1,5 +1,13 @@
+// eslint-disable-next-line camelcase
+const selenium_port = process.env.SELENIUM_PORT || 4444
+
 module.exports = {
+  test_settings: {
+    default: {
+      selenium_port
+    }
+  },
   'selenium': {
-    'port': process.env.SELENIUM_PORT || 4444
+    'port': selenium_port
   }
 }
